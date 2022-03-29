0abf2f1372a2b5205993305449d9a6ca9ac8354b
Beni Morales <i.benimorales@gmail.com>
2022-03-29 12:44:06 +0200

    Function toggleCompleted fixed

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -15,7 +15,7 @@ export default function ToDoList({ toDo, setToDoList }) {
             {
                 toDo.map(({ title }, index) => {
                     return <div key={index} className="row mb-2">
-                        <li id={index} onClick={event => toggleCompleted(event, index)}
+                        <li id={index} onClick={event => toggleCompleted(index)}
                             className={toDo[index].completed ? "list-group-item col-11 completed" : "list-group-item col-11"}>{index}: {title}
                         </li>
                         <button id={title} onClick={() => deleteTask(title)} className="btn btn-danger col-1">x</button>
